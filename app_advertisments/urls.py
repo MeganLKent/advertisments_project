364e1af34d1a0c2513c78e18a78e4db052c83db1
MeganLKent <137336310+MeganLKent@users.noreply.github.com>
2023-08-18 19:46:20 +0300

    Add files via upload

--- app_advertisments/urls.py
+++ app_advertisments/urls.py
@@ -1,7 +1,8 @@
 from django .urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisment_post
 
 urlpatterns = [
     path('', index, name='main-page'),
-    path('top-sellers/', top_sellers, name = 'top-sellers')
+    path('top-sellers/', top_sellers, name = 'top-sellers'),
+    path('advertisment_post/', advertisment_post, name = 'adv-post')
 ]
--- app_advertisments/views.py
+++ app_advertisments/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Advertisment
+from .forms import AdvertismentForm
+from django.urls import reverse
+
 
 def index(request):
     advertisments = Advertisment.objects.all()
@@ -9,3 +12,17 @@ def index(request):
 
 def top_sellers(request):
     return render (request, 'top-sellers.html')
+
+def advertisment_post(request):
+    if request.method == 'POST':
+        form = AdvertismentForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisment = Advertisment(**form.cleaned_data)
+            advertisment.user = request.user
+            advertisment.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertismentForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
